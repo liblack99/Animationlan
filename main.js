81f5253747f4944c1ac40b585813530c25764b5e
andres <andresfc-@hotmail.com>
2023-09-01 14:00:17 -0500

    add(incremento de velocidad)

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
-const rabbit = document.querySelectorAll(".rabbit");
+const rabbits = document.querySelectorAll(".rabbit");
 const score = document.getElementById("score");
 
 let count = 0;
 
-rabbit.forEach((element) => {
-  element.addEventListener("click", () => {
+rabbits.forEach((rabbit) => {
+  rabbit.addEventListener("click", () => {
+    const currentSpeed = parseFloat(getComputedStyle(rabbit).animationDuration);
+    const newSpeed = currentSpeed - 0.1;
+
     count = count + 1;
     score.textContent = count;
+
+    rabbit.style.animationDuration = `${newSpeed}s`;
   });
 });
